8aeab999ffbfa12b803923167a1424e943735976
Vita <71135942+eogml0807@users.noreply.github.com>
2020-12-23 10:06:28 +0900

    요구사항 이행 / 테스트 시 알려주세용

--- Delivery-Web/src/main/java/com/kh/delivery/controller/UserController.java
+++ Delivery-Web/src/main/java/com/kh/delivery/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.kh.delivery.controller;
 
 import javax.inject.Inject;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.delivery.domain.UserVo;
 import com.kh.delivery.service.UserService;
@@ -16,36 +22,57 @@ public class UserController {
 	
 	@Inject
 	private UserService userService;
+	@Inject
+	private JavaMailSender mainSender;
 	
-	/*로그인 , 회원가입, 아이디or비밀번호 찾기 */
+	// 웹
 	
+	// 로그인
 	@RequestMapping(value="/loginForm")
 	public String loginForm(String user_id, String user_pw) throws Exception {
 		return "pages/loginForm";
 	}
 	
-	@RequestMapping(value="/loginRun")
+	@RequestMapping(value="/loginRun", method=RequestMethod.POST)
 	public String loginRun(String user_id, String user_pw, HttpSession session) throws Exception {
-		System.out.println(user_id + user_pw);
+		System.out.println("loginRun, user_info = " + user_id + ", " + user_pw);
 		UserVo userVo = userService.login(user_id, user_pw);
-		
+		System.out.println("loginRun, userVo = " + userVo);
 		if(userVo != null) {
 			session.setAttribute("userVo", userVo);
 			return "redirect:/";
-		} else {}
-		
-		return "pages/loginForm";
+		} else {
+			session.setAttribute("login_result", "login_fail");
+			return "redirect:/user/loginForm";
+		}
 	}
 	
+	
+	// 아이디 중복 체크
+	@RequestMapping(value="/checkIdDupl", method=RequestMethod.GET)
+	@ResponseBody
+	public boolean checkIdDupl(String user_id) throws Exception {
+		System.out.println("checkDulp, user_id = " + user_id);
+		boolean result = userService.checkIdDupl(user_id);
+		return result;
+	}
+	
+	
+	//회원 가입
 	@RequestMapping(value="/registerForm")
-	public String registerForm(UserVo userVo) throws Exception {
+	public String registForm() throws Exception {
 		return "pages/registerForm";
 	}
 	
 	@RequestMapping(value="/registerRun")
-	public void registerRun(UserVo userVo) throws Exception {
-		
+	public String registRun(UserVo userVo, RedirectAttributes rttr) throws Exception {
+		System.out.println(userVo);
+		String result = userService.registUser(userVo);
+		System.out.println("result = " + result);
+		rttr.addFlashAttribute("msg", result);
+		return "redirect:/";
 	}
+	
 	
 	/*주소 검색*/
 	@RequestMapping(value="/address", method=RequestMethod.GET)
@@ -53,21 +80,32 @@ public class UserController {
 		return "util/address";
 	}
 	
-	/*아이디&비밀번호 찾기 Form*/
+	// 아이디, 비밀번호 찾기
 	@RequestMapping(value="/findAccountForm", method=RequestMethod.GET)
-	public String findAccountForm() {
+	public String findAccountForm() throws Exception {
 		return "pages/findAccountForm";
 	}
 	
-	/*아이디&비밀번호 찾기 Run*/
 	@RequestMapping(value="/findAccountRun", method=RequestMethod.POST)
-	public void findAccountRun(String user_name, String user_email, String user_phone){
-		// user_name , user_email, user_phone 자료 정상적으로 넘어옴 
-		
-		System.out.println("UserController, findAccountRun, user_name:" + user_name);
-		System.out.println("UserController, findAccountRun, user_email:" + user_email);
-		System.out.println("UserController, findAccountRun, user_phone:" + user_phone);
+	public String findAccountRun(String user_name, String user_phone, String user_email, RedirectAttributes rttr) throws Exception {
+		System.out.println("findAccount, user_info = " + user_name + ", " + user_phone + ", " + user_email);
+		UserVo userVo = userService.findAccount(user_name, user_phone, user_email);
+		System.out.println(userVo);
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
 			
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+				helper.setFrom("eogml0807@gmail.com");
+				helper.setTo(userVo.getUser_email());
+				helper.setSubject("계정 정보");
+				helper.setText("아이디 = " + userVo.getUser_id() + ", 비밀번호 = " + userVo.getUser_pw());
+			}
+		};
+		
+		mainSender.send(preparator);
+		rttr.addFlashAttribute("msg", "send email");
+		return "redirect:/";
 	}
 	
 }
