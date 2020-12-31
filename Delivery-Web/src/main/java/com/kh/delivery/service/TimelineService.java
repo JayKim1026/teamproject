9bc32966e74a70652da099b78c8813390917c6dd
Vita <71135942+eogml0807@users.noreply.github.com>
2020-12-31 15:56:58 +0900

    타임라인 수정3 201231 1556

--- Delivery-Web/src/main/java/com/kh/delivery/service/TimelineService.java
+++ Delivery-Web/src/main/java/com/kh/delivery/service/TimelineService.java
@@ -17,4 +17,14 @@ public interface TimelineService {
 	
 	/*글 삭제*/
 	public void deleteArticle(int review_no);
+	
+	// 수정본
+	/* 글 리스트 */
+	public List<TimelineVo> timelineList2(); 
+	/*글 작성*/
+	public String insertArticle2(TimelineVo timelineVo);
+	/*글 수정*/
+	public String updateArticle2(TimelineVo timelineVo);
+	/*글 삭제*/
+	public String deleteArticle2(int time_no);
 }
