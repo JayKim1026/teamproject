e122c245ca1b940b22e547c98bb01c8f373972a0
Vita <71135942+eogml0807@users.noreply.github.com>
2021-01-25 11:29:04 +0900

    ...

--- Delivery-Web/src/main/java/com/kh/delivery/controller/AccountController.java
+++ Delivery-Web/src/main/java/com/kh/delivery/controller/AccountController.java
@@ -25,32 +25,33 @@ public class AccountController {
 	JavaMailSender mailSender;
 	@Inject
 	AccountService accountService;
-	
+
+	// 로그인 페이지
+	@RequestMapping(value = "/loginForm", method = RequestMethod.GET)
+	public String loginForm() throws Exception {
+		return "account/loginForm";
+	}
+	// 사용자 회원가입 페이지
+	@RequestMapping(value = "/registerForm", method = RequestMethod.GET)
+	public String registForm() throws Exception {
+		return "account/registerForm";
+	}
+	// 배달원 회원가입 페이지
+	@RequestMapping(value = "/dlvr_RegisterForm", method = RequestMethod.GET)
+	public String dlvr_RegisterForm() throws Exception {
+		return "account/dlvr_RegisterForm";
+	}
+	// 아이디, 비밀번호 찾기 페이지
+	@RequestMapping(value = "/findAccountForm", method = RequestMethod.GET)
+	public String findAccountForm() throws Exception {
+		return "account/findAccountForm";
+	}
 	// 로그아웃
 	@RequestMapping(value="/logout" , method=RequestMethod.GET)
 	public String logout(HttpSession session) throws Exception {
 		session.invalidate();
 		return "redirect:/";
 	}
-
-	// 로그인 화면
-	@RequestMapping(value = "/loginForm", method = RequestMethod.GET)
-	public String loginForm() throws Exception {
-		return "account/loginForm";
-	}
-
-	// 사용자 회원가입
-	@RequestMapping(value = "/registerForm", method = RequestMethod.GET)
-	public String registForm() throws Exception {
-		return "account/registerForm";
-	}
-
-	// 배달원 회원가입
-	@RequestMapping(value = "/dlvr_RegisterForm", method = RequestMethod.GET)
-	public String dlvr_RegisterForm() throws Exception {
-		return "account/dlvr_RegisterForm";
-	}
-
 	// 아이디 중복확인
 	@RequestMapping(value = "/checkIdDupl", method = RequestMethod.POST)
 	@ResponseBody
@@ -58,13 +59,6 @@ public class AccountController {
 		boolean result = accountService.checkIdDupl(acc_id);
 		return result;
 	}
-
-	// 아이디, 비밀번호 찾기
-	@RequestMapping(value = "/findAccountForm", method = RequestMethod.GET)
-	public String findAccountForm() throws Exception {
-		return "account/findAccountForm";
-	}
-
 	// 아이디 찾기
 	@RequestMapping(value = "/findAccountId", method = RequestMethod.POST)
 	@ResponseBody
@@ -72,7 +66,6 @@ public class AccountController {
 		String acc_id = accountService.findAccountId(accountDto);
 		return acc_id;
 	}
-
 	// 비밀번호 찾기
 	@RequestMapping(value = "/sendAccountPw", method = RequestMethod.POST)
 	@ResponseBody
@@ -80,7 +73,6 @@ public class AccountController {
 		String acc_pw = accountService.findAccountPw(accountDto);
 		String acc_email = accountDto.getAcc_email();
 		MimeMessagePreparator preparator = new MimeMessagePreparator() {
-
 			@Override
 			public void prepare(MimeMessage mimeMessage) throws Exception {
 				String text = "비밀번호는 " + acc_pw + " 입니다.";
@@ -95,7 +87,7 @@ public class AccountController {
 		mailSender.send(preparator);
 		return "sendAccountPw_success";
 	}
-	
+	// 포인트 랭킹 조회
 	@RequestMapping(value="/getPointRank", method=RequestMethod.POST)
 	@ResponseBody
 	public List<AccountDto> getPointRank() throws Exception {
