275f51a4f5be5c58f63e3860c9ed6f71c7aed21b
Vita <71135942+eogml0807@users.noreply.github.com>
2021-01-26 11:11:55 +0900

    adminInterceptor 추가 210126 1111 박대희

---
+++ Delivery-Web/src/main/java/com/kh/delivery/interceptor/AdminInterceptor.java
@@ -0,0 +1,32 @@
+package com.kh.delivery.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.kh.delivery.dao.AdminDao;
+import com.kh.delivery.domain.AdminVo;
+
+public class AdminInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		AdminVo adminVo = (AdminVo) session.getAttribute("adminVo");
+		if(adminVo == null) {
+			response.sendRedirect("/");
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		super.postHandle(request, response, handler, modelAndView);
+	}
+}
